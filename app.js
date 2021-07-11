d928b399fa8849aebdfe06ac667e2b4c0700003f
KevinArmstrong-dev <krwigamba@gmail.com>
2021-07-11 13:26:46 -0400

    added api request on the weather stack api

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const request = require('request');
 
-const url = ''
+const url = 'http://api.weatherstack.com/current?access_key=eacbe4128b041bb2b9a09b8cbb0b5b7a&query=New%20York'
+
+request({url: url,json:true},(error, response)=> {
+    console.log(response.body.current);
+})  
