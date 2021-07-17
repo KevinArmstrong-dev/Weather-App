c6a092fad572c9e158f173fafb765809016b6ca3
KevinArmstrong-dev <krwigamba@gmail.com>
2021-07-17 16:06:12 -0400

    Finished the challenge

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const request = require('request');
 const url = 'http://api.weatherstack.com/current?access_key=eacbe4128b041bb2b9a09b8cbb0b5b7a&query=New%20York'
 
 //This for the mapbox api
-const GeoApiUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?access_token=pk.eyJ1Ijoia2V2aW4tYXJtc3Ryb25nIiwiYSI6ImNrcjVydHAxeDM4dnEyd3Q5eTlrbThhaGsifQ.xhXs7iKVrk8kIm9BsymZiA";
+const GeoApiUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2V2aW4tYXJtc3Ryb25nIiwiYSI6ImNrcjVydHAxeDM4dnEyd3Q5eTlrbThhaGsifQ.xhXs7iKVrk8kIm9BsymZiA";
 
 
 request({url: url,json:true},(error, response)=> {
-    console.log(response.body.current);
-    console.log('It is currently '+response.body.current.temperature+ " degrees out. There is a "+ response.body.current.precip *100 + "% chance of rain");
+    //console.log(response.body.current);
+    //console.log('It is currently '+response.body.current.temperature+ " degrees out. There is a "+ response.body.current.precip *100 + "% chance of rain");
 })  
 
 request({url: GeoApiUrl,json:true},(error, response)=> {
-    console.log("Geo Api says" + response[0]);
-   // console.log('It is currently '+response+ " degrees out. There is a "+ response.body.current.precip *100 + "% chance of rain");
+   // console.log("Geo Api says " + response.body.features[2].geometry.coordinates);
+    const coordinates = response.body.features[2].geometry.coordinates;
+    console.log(" The coordinates are  "+ coordinates[0] + " and "+coordinates[1]);
 })  
