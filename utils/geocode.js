c62b9b9c30622017692515e3828e3366e5bdfe1c
KevinArmstrong-dev <krwigamba@gmail.com>
2021-07-28 14:33:55 -0400

    finished setting the forecast util function

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,47 +1,5 @@
 const request = require('request');
 
-setTimeout(()=>{
-    console.log("Two seconds are up");
-},2000);
-
-const names = ["Andrew,", "Kevin","Jenny"];
-
-const shortNames = names.filter((name)=> {
-    return name.length <= 5;
-})
-
-const geocode = (address, callback) => {
-   setTimeout(()=>{
-    const data = { 
-        latitude:0,
-        longitude:0
-    }
-
-   callback(data);
-
-   },2000)
-
-}
-
-const data = geocode ('Montreal', (data) =>{
-    console.log(data);
-
-});
-
-
-function sumUp(a, b) {console.log(a+b)}
-
-const add = (a, b, callback) =>{
-    setTimeout(()=>{
-
-       callback(a, b);
-    
-       },2000)
-}
-
-
-add(1,3,sumUp);
-
 const geocode = (address, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address)+".json?access_token=pk.eyJ1Ijoia2V2aW4tYXJtc3Ryb25nIiwiYSI6ImNrcjVydHAxeDM4dnEyd3Q5eTlrbThhaGsifQ.xhXs7iKVrk8kIm9BsymZiA";
 
@@ -58,3 +16,11 @@ const geocode = (address, callback) => {
             })
         }
     })
+
+}
+
+geocode('Montreal', (error,data) =>{
+    console.log('Error', error);
+    console.log('Data', data);
+});
+module.exports =  geocode
