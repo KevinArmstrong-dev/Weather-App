722500e8c37ba2d81e907b04cb698ea761da6ac2
KevinArmstrong-dev <krwigamba@gmail.com>
2021-08-27 14:51:00 -0400

    save

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -1,4 +1,4 @@
-const request = require('request');
+
 
 setTimeout(()=>{
     console.log("Two seconds are up");
@@ -41,20 +41,3 @@ const add = (a, b, callback) =>{
 
 
 add(1,3,sumUp);
-
-const geocode = (address, callback) => {
-    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address)+".json?access_token=pk.eyJ1Ijoia2V2aW4tYXJtc3Ryb25nIiwiYSI6ImNrcjVydHAxeDM4dnEyd3Q5eTlrbThhaGsifQ.xhXs7iKVrk8kIm9BsymZiA";
-
-    request ({url:url,json:true},(error,response) => {
-        if(error){
-            callback('Unable to connect to Loaction services',undefined);
-        }else if(response.body.features.length === 0 ){
-            callback('Unable to find location, try another search',undefined)
-        } else{
-            callback(undefined,{
-                latitude: response.body.features[0].center[0],
-                longitude:response.body.features[0].center[1],
-                location: response.body.features[0].place_name
-            })
-        }
-    })
